148ff3e7d95dc94b89ffc00d05dc269af902d61b
shkim <sunghunk@gmail.com>
2017-03-16 23:35:31 +0900

    텐서보드 기능 추가

--- 02.tensorflow/20170316_02_mnist_fcwt_02.py
+++ 02.tensorflow/20170316_02_mnist_fcwt_02.py
@@ -9,10 +9,10 @@ import matplotlib.pyplot as plt
 
 mnist = input_data.read_data_sets("./mnist_data", one_hot=True)
 
-x = tf.placeholder(tf.float32, [None, 784])
-y_ = tf.placeholder(tf.float32, [None, 10])
+x = tf.placeholder(tf.float32, [None, 784], name="X-input")
+y_ = tf.placeholder(tf.float32, [None, 10], name="Y-input")
 
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+x_image = tf.reshape(x, [-1, 28, 28, 1], name="X-input-image")
 
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
@@ -31,43 +31,67 @@ def max_pool_2x2(x):
 w_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+with tf.name_scope("conv1") as scope:
+    h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
 w_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+with tf.name_scope("conv2") as scope:
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
 w_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
+with tf.name_scope("fc1") as scope:
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 
-keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 w_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+#y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+#cross_entropy = tf.reduce_sum(y_ * tf.log(y_conv))
 
-cross_entropy = tf.reduce_sum(y_ * tf.log(y_conv))
+y_conv = tf.matmul(h_fc1_drop, w_fc2) + b_fc2
+
+with tf.name_scope("cost") as scope:
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    cross_summ = tf.summary.scalar("cost", cross_entropy)
+
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y_conv, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+with tf.name_scope("accuracy") as scope:
+    correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y_conv, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy_summ = tf.summary.scalar("accuracy", accuracy)
 
 sess = tf.InteractiveSession()
-tf.global_variables_initializer().run()
+merged = tf.summary.merge_all()
+writer = tf.summary.FileWriter("./logs/xor_logs", sess.graph_def)
 
-for i in range(20000):
+sess.run(tf.global_variables_initializer())
+
+costs = []
+for i in range(2000):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
-        train_accuracy = sess.run( accuracy, feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g"%(i, train_accuracy))
+        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        train_cross_entropy = sess.run(cross_entropy, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, train_cross_entropy %f, training accuracy %g" % (i, train_cross_entropy, train_accuracy))
+        costs.append(train_cross_entropy)
+        summary = sess.run(merged, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+        writer.add_summary(summary, i)
     sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g" % sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.lables, keep_prob: 1.0}))
+print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+plt.plot(range(len(costs)), costs)
+plt.ylim(0, 1)
+plt.show()
